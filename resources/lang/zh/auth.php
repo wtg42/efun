b6f467fdeb9c44ffd3bc440c6da34fe835ecda05
weiting <weiting.shi1982@gmail.com>
2021-04-02 10:29:13 +0800

    New dashboard and zh auth language messages

---
+++ resources/lang/zh/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '身份認證不通過。',
+    'password' => '提供的密碼並不正確。',
+    'throttle' => '登入太多次。 請在 :seconds 秒後再嘗試。',
+
+];
