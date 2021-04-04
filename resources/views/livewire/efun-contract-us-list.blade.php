5115412b7069a7040bea71b62a4ab9c23af17e68
weiting <weiting.shi1982@gmail.com>
2021-04-04 10:14:19 +0800

    Contract us page in workbench

--- resources/views/livewire/efun-contract-us-list.blade.php
+++ resources/views/livewire/efun-contract-us-list.blade.php
@@ -14,7 +14,7 @@
                     Message</th>
                 <th
                     class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                    Last show time</th>
+                    Date On Site</th>
             </tr>
         </thead>
         <tbody>
