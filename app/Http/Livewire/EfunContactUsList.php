ad6fdc154639d5e32b26fb3a3521180f71a3dec5
weiting <weiting.shi1982@gmail.com>
2021-04-04 22:27:47 +0800

    Rename livewire component

--- app/Http/Livewire/EfunContactUsList.php
+++ app/Http/Livewire/EfunContactUsList.php
@@ -5,14 +5,14 @@ namespace App\Http\Livewire;
 use App\Models\ContactUs;
 use Livewire\Component;
 
-class EfunContractUsList extends Component
+class EfunContactUsList extends Component
 {
     public $userList;
     public function render()
     {
         $this->userList = ContactUs::all();
 
-        return view('livewire.efun-contract-us-list', $this->userList);
+        return view('livewire.efun-contact-us-list', $this->userList);
     }
 }
 // to-do wire:click刪除紀錄
