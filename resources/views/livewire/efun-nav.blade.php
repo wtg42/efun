abd45f419ca6b3a067df4f84ab4b42b47e790e28
weiting <weiting.shi1982@gmail.com>
2021-04-04 20:54:02 +0800

    New nav bar

--- resources/views/livewire/efun-nav.blade.php
+++ resources/views/livewire/efun-nav.blade.php
@@ -1,20 +1,23 @@
 {{-- Class EfunNav --}}
-<nav class="fixed z-50 w-full bg-white top-0 flex flex-wrap items-center justify-between px-2 py-3 shadow-lg">
+<nav class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3 ">
     <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
         <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-blueGray-700" href="https://www.facebook.com/%E4%B8%80%E5%B8%86%E5%A0%B1%E9%97%9C%E8%B3%87%E8%A8%8A%E8%A1%8C-351932298606336/">EFUN CUSTOMS BROKER</a>
-            <button class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
-            <i class="fas fa-bars"></i>
+            <a target="_blank" class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+                href="https://www.facebook.com/%E4%B8%80%E5%B8%86%E5%A0%B1%E9%97%9C%E8%B3%87%E8%A8%8A%E8%A1%8C-351932298606336/">一帆報關資訊行</a><button
+                class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+                type="button" onclick="toggleNavbar('example-collapse-navbar')">
+                <i class="text-white fas fa-bars"></i>
             </button>
         </div>
-        <div class="lg:flex flex-grow items-center hidden" id="example-navbar-danger">
-        <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
-            <li class="nav-item">
-            <a href="https://www.facebook.com/%E4%B8%80%E5%B8%86%E5%A0%B1%E9%97%9C%E8%B3%87%E8%A8%8A%E8%A1%8C-351932298606336/" class="px-3 py-2 flex items-center text-xs uppercase font-bold text-blueGray-700 hover:text-blueGray-500" target="_blank">
-                <i class="fab fa-facebook-square text-lg leading-lg text-blueGray-400"></i>
-                <span class="ml-2">聯絡我們</span>
-            </a>
-            </li>
+        <div class="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none hidden"
+            id="example-collapse-navbar">
+            <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
+                <li class="flex items-center">
+                    <a target="_blank" class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
+                        href="https://www.facebook.com/%E4%B8%80%E5%B8%86%E5%A0%B1%E9%97%9C%E8%B3%87%E8%A8%8A%E8%A1%8C-351932298606336/"><i
+                            class="lg:text-gray-300 text-gray-500 fab fa-facebook text-lg leading-lg "></i><span
+                            class="lg:hidden inline-block ml-2" target="_blank">Share</span></a>
+                </li>
             </ul>
         </div>
     </div>
