ee4272206883df696aa49baa78636bcb85f3f1ad
weiting <weiting.shi1982@gmail.com>
2021-05-04 10:59:21 +0800

    資料分頁改用Livewire，修正login history排序跟資料錯誤。

--- app/Http/Livewire/EfunUserLoginHistory.php
+++ app/Http/Livewire/EfunUserLoginHistory.php
@@ -3,14 +3,19 @@
 namespace App\Http\Livewire;
 
 use App\Models\User;
+use App\Models\UserLog;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class EfunUserLoginHistory extends Component
 {
+    use WithPagination; // livewire分頁用 沒加會變成reload分頁
+
     public function render()
     {
         // with() 吃orm function name
-        $userLoginHistory = User::with('userLog')->paginate(1);
+        // $userLoginHistory = User::with('userLog')->paginate(1);
+        $userLoginHistory = UserLog::with('user')->orderBy('login_time', 'desc')->paginate(4);
         return view('livewire.efun-user-login-history', ['userLoginHistory' => $userLoginHistory]);
     }
 }
